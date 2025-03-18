dea4840f381d535f22ca372edd51d8fa1af372f1
Trevor Norton <nortrevor02@gmail.com>
2025-03-18 15:09:28 -0500

    Finalized T1 Implementation of Wearable System

--- Scenes/clothing_item.gd
+++ Scenes/clothing_item.gd
@@ -24,4 +24,17 @@ var Pose28: Texture2D
 func _ready() -> void:
 	attackDamageChange = wearable_resource.get_attack_damage_change()
 	stylePoints = wearable_resource.get_style_points()
-	#TODO finish this shit
+	styleMultiplier = wearable_resource.get_style_multiplier()
+	speedChange = wearable_resource.get_speed_change()
+	
+	outfitSet = wearable_resource.get_outfit_set()
+	outfitPattern = wearable_resource.get_outfit_pattern()
+	
+	idlePose = wearable_resource.get_idle_pose()
+	walkPose1 = wearable_resource.get_walk_pose1();
+	blockPose = wearable_resource.get_block_pose();
+	kickPose = wearable_resource.get_kick_pose();
+	punchPose = wearable_resource.get_punch_pose();
+	hurtPose = wearable_resource.get_hurt_pose();
+	Pose28 = wearable_resource.get_pose28();
+	
