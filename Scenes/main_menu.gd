65fcc82a3eb2d7b93f8005bd2d05314afd39d540
tyler-ross26 <14wildnoodles@gmail.com>
2025-03-18 23:31:07 -0500

    Update
    Basic functionality added to main menu

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_settings_pressed() -> void:
+	print("Settings pressed")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
