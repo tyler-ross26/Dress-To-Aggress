756887249e9d4edf7169821b1bed2bc441436b0c
Trevor Norton <nortrevor02@gmail.com>
2025-03-30 21:44:38 -0500

    Updated Compatability after Branch Merges

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/DressUp.tscn")
 
 
 func _on_settings_pressed() -> void:
